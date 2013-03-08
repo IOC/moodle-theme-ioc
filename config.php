70f975e2a0d744ac83f3c151f9ac01be136dfc27
Albert Gasset <albert.gasset@gmail.com>
2013-03-08 09:52:58 +0100

    Initial commit

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+
+$THEME->name = 'ioc';
+$THEME->parents = array('leatherbound', 'canvas', 'base');
+$THEME->sheets = array('ioc');
+$THEME->enable_dock = true;
---
+++ version.php
@@ -0,0 +1,5 @@
+<?php
+
+$plugin->version = 2012092000;
+$plugin->requires  = 2012061700;
+$plugin->component = 'theme_ioc';
