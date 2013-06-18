e9102600de331022136e76b496393ef8b11dd51f
Albert Gasset <albert.gasset@gmail.com>
2013-06-18 12:24:40 +0200

    No block regions in base layout

--- config.php
+++ config.php
@@ -4,3 +4,10 @@ $THEME->name = 'ioc';
 $THEME->parents = array('leatherbound', 'canvas', 'base');
 $THEME->sheets = array('ioc');
 $THEME->enable_dock = true;
+
+$THEME->layouts = array(
+    'base' => array(
+        'file' => 'general.php',
+        'regions' => array(),
+    ),
+);
