3bcec7b54604b9a4403045f5e93c6c2763d8fc0b
Marc Català <reskit@gmail.com>
2013-09-04 12:27:57 +0200

    Added choosereadme lang string

--- lang/en/theme_ioc.php
+++ lang/en/theme_ioc.php
@@ -1,3 +1,4 @@
 <?php
 
+$string['choosereadme'] = '<div><p>Ioc is a modified Moodle leatherbound theme which inherits styles and renderers from its parent theme.</p></div>';
 $string['pluginname'] = 'IOC';
